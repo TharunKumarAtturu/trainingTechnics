b8d8caa950afafdcc8e33abc8690fd7b19d3edbc
TharunKumarAtturu <78899436+TharunKumarAtturu@users.noreply.github.com>
2021-02-24 17:08:04 +0530

    framework_python(v1.2).py
    "coded as python_idiom."

--- framework_python.py
+++ framework_python.py
@@ -1,32 +1,30 @@
 # Framework to perform create and read operation(using lists to store the data).
+import os
 FIELDS_FILE = 'fields.cfg'
 MENU_FILE = 'menu.cfg'
 DATA_FILE = 'data.dat'
 records = []
+fields = eval(open(FIELDS_FILE).read())
 
 def store_temporary_record_list():
-	""" Storing the existing data before overwrite records into data file."""
-	with open(DATA_FILE, "r") as temp_lists:
-		temp_records = temp_lists.read()
-		temp_list_from_data_file = eval(temp_records)
-		for line in temp_list_from_data_file:
-			records.append(line)
+	""" checking whether the data file is empty,
+	    if empty instering a empty list,
+	    if not storing the records it in a list. """
+	if os.stat(DATA_FILE).st_size == 0:
+		empty_list = []
+		with open(DATA_FILE, "a") as file_data:
+			file_data.write(str(empty_list))
+		# eval(open(DATA_FILE, "w").write(str(empty_list)))
 
-with open(FIELDS_FILE, "r") as fields_file:
-	fields_list = fields_file.read()
-fields = eval(fields_list)
-
-with open(MENU_FILE, "r") as menu_file:
-	menu_list = menu_file.read()
-menu_lines = eval(menu_list)
+	else:
+		[records.append(line) for line in eval(open(DATA_FILE).read())]
 
 def create_record():
 	"""Create a record corresponding to fields."""
+	record_temp = []
+	[record_temp.append(input("Enter the "+ field + ": ")) for field in fields]
+	records.append(record_temp)
 	with open(DATA_FILE, "w") as data_file:
-		record_temp = []
-		for field in fields:
-			record_temp.append(input("Enter the " + field + ": "))
-		records.append(record_temp)
 		data_file.write(str(records))
 
 def read_records():
@@ -34,14 +32,11 @@ def read_records():
 	for field in fields:
 		print(field, end = " ")
 	print("\n")
-	with open(DATA_FILE, "r") as data_file:
-		records = data_file.read()
-		temp_records = eval(records)
-		for lines in temp_records:
+	temp_records = eval(open(DATA_FILE, "r").read())
+	for lines in temp_records:
 			for line in lines:
-				print(line, end = " ",)
-
-			print("\n")	
+				print(line, end = " ")
+			print("\n")
 
 def exit_from_menu():
 	"""Exit from the Main Menu."""
@@ -51,8 +46,8 @@ def exit_from_menu():
 def show_main_menu():
 	"""Open's the menu file and displays the Main Menu."""
 	while True:
-		for menu_line in menu_lines:
-			print(menu_line)
+		# print("\n")
+		print(open(MENU_FILE).read())
 		[create_record, read_records, exit_from_menu][int(input("Enter your option: ")) - 1]()
 
 store_temporary_record_list()
