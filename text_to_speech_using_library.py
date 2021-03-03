26c610de6ed5f31023ef7dc469b5cbc229f78821
TharunKumarAtturu <78899436+TharunKumarAtturu@users.noreply.github.com>
2021-03-03 17:16:01 +0530

    Update text_to_speech_using_library.py

--- text_to_speech_using_library.py
+++ text_to_speech_using_library.py
@@ -1,10 +1,8 @@
+'''Text to S '''
 import pyttsx3
 engine = pyttsx3.init()
 text = []
 given_text = input("Enter a number: ")
-for x in given_text:
-	text.append(x)
-	text.append(" ")
-
+[text.append(x + " ") for x in given_text]
 engine.say(text)
 engine.runAndWait()
