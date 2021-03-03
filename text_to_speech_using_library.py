d077b897412b48fd7d4a7cc7fecbe136508d782a
TharunKumarAtturu <78899436+TharunKumarAtturu@users.noreply.github.com>
2021-03-03 17:44:52 +0530

    Update text_to_speech_using_library.py

--- text_to_speech_using_library.py
+++ text_to_speech_using_library.py
@@ -3,6 +3,14 @@ import pyttsx3
 engine = pyttsx3.init()
 text = []
 given_text = input("Enter a number: ")
-[text.append(x + " ") for x in given_text]
-engine.say(text)
-engine.runAndWait()
+def text_to_speech_using_library():
+  try:
+    [text.append(x + " ") for x in given_text]
+    engine.say(text)
+    engine.runAndWait()
+
+  except Exception as error:
+	  print("Error!", error)
+	  print("Text to speech failed!")
+  
+text_to_speech_using_library()
