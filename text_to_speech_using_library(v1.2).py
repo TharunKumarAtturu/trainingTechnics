4d500af4537180b03ec9c86a2843ff91d11693f9
TharunKumarAtturu <78899436+TharunKumarAtturu@users.noreply.github.com>
2021-03-03 19:00:18 +0530

    Update text_to_speech_using_library(v1.2).py

--- text_to_speech_using_library(v1.2).py
+++ text_to_speech_using_library(v1.2).py
@@ -1,8 +1,8 @@
 '''Text to Speech using library(pyttsx3).'''
 import pyttsx3
 engine = pyttsx3.init()
-number = []
 given_number = input("Enter a number: ")
+
 def text_to_speech_using_library():
   try:
     [engine.say(digit) for digit in given_number]
