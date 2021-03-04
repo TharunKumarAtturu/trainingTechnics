376cce2a02f6cc27b612a8bf87ab4f915f86b477
TharunKumarAtturu <78899436+TharunKumarAtturu@users.noreply.github.com>
2021-03-04 11:37:04 +0530

    Update conversion_of_rgb_image_into_gray_image_and_black_and_white_image.py

--- conversion_of_rgb_image_into_gray_image_and_black_and_white_image.py
+++ conversion_of_rgb_image_into_gray_image_and_black_and_white_image.py
@@ -1,10 +1,10 @@
-''' Conversion of BGR Image into Gray Image and Black and White Image'''
+''' Conversion of Original (BGR) Image into Gray Image and Black and White Image.'''
 
 import cv2
 IMAGE_FILE_NAME = 'original_image.png'
 original_image = cv2.imread(IMAGE_FILE_NAME)
 try:
-  '''Converts given BGR image into gray image and balck and white image.'''
+  '''Converts given original (BGR) Image into Gray Image and Balck and White Image.'''
 	gray_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
   
 	(thresh, black_and_white_image) = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)
@@ -15,7 +15,7 @@ except Exception as error:
 
 
 else:
-  '''Displays the original image, converted gray image and converted black and image/'''
+  '''Displays the Original Image (BGR), converted Gray Image and converted Black and Image.'''
 	cv2.imshow('Black And White image', black_and_white_image)
 	cv2.imshow('Original image',original_image)
 	cv2.imshow('Gray image', gray_image)
