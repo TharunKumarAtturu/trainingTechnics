ceda09d2c3832e40de0a07763fdee4b81c4c286f
TharunKumarAtturu <78899436+TharunKumarAtturu@users.noreply.github.com>
2021-03-04 12:00:09 +0530

    Update text_to_speech_using_audio_files.py

--- text_to_speech_using_audio_files.py
+++ text_to_speech_using_audio_files.py
@@ -1,7 +1,7 @@
 ''' Text to Speech using audio files. '''
 from playsound import playsound 
 given_number = input("Enter a number: ")
-def get_audio_from_text():
+def convert_text_to_audio():
 	try:
 		[playsound(digit + '.wav') for digit in given_number]
 
@@ -9,4 +9,4 @@ def get_audio_from_text():
 		print("Error!", error)
 		print("Text to speech failed!")
 
-get_audio_from_text()
+convert_text_to_audio()
